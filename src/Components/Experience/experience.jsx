1ffedc3d02d1a0bd3871a7d09b7ad71cb49d64cb
Rajeev Kushwaha <rk7940271@gmail.com>
2023-06-13 23:10:43 +0530

    made some changes

--- src/Components/Experience/experience.jsx
+++ src/Components/Experience/experience.jsx
@@ -18,7 +18,7 @@ const experience = () => {
         </div>
 
         <div className="achievement">
-            <div className="circle">2+</div>
+            <div className="circle">3+</div>
             <span>Hackathons</span>
             <span>Attend</span>
         </div>
