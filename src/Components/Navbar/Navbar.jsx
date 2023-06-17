e984f96a1f39c1c1fc51e7ca95d388e8591791d9
Rajeev Kushwaha <rk7940271@gmail.com>
2023-06-17 14:37:29 +0530

    sidebar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,18 +2,45 @@ import React from 'react'
 import './Navbar.css'
 import Toogle from '../Toggle/Toggle'
 import {Link} from 'react-scroll'
+import Bar from '@iconscout/react-unicons/icons/uil-bars'
+import Cross from '@iconscout/react-unicons/icons/uil-times'
+import { useState } from 'react';
+
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const handleMobileMenuToggle = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
   return (
-    <div className="n-wrapper">
+    <div className="n-wrapper" id='Navbar'>
         <div className="n-left">
-           <div className="n-name">Rajeev</div> 
-           <Toogle/>
+            <div className="n-name">Rajeev</div> 
+            <Toogle/>
+            <div
+					className='menu-icon'
+                    onClick={handleMobileMenuToggle}
+					>
+					<Bar  color='orange' size='2.5rem' />
+			</div>
+
+            {/* <div className="mobile-menu-toggle" onClick={handleMobileMenuToggle}>
+        {isMobileMenuOpen ? 'Close' : 'Menu'}
+      </div> */}
+
         </div>
         
-        <div className="n-right">
+        <div className={`n-right ${isMobileMenuOpen ? 'mobile-menu' : 'n-right'}`}>
             <div className="n-list">
                 <ul>
+                <div
+					className='cross-icon'
+                    onClick={handleMobileMenuToggle}
+					>
+                    <Cross  color='orange' size='2.5rem' />
+			    </div>
+
                     <Link spy={true} to='Navbar' smooth={true} activeClass= 'activeClass'>
                     <li className='h-effect'>Home</li>
                     </Link>
